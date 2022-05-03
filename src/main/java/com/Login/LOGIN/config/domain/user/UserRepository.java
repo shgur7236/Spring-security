3631e69342876af75b247cd38df335437fa00c08
shgur7236 <shgurtns7236@naver.com>
2022-05-03 23:13:41 +0900

    UserRepository생성 JpaRepository 상속받아주고 이메일을 통해 회원을 조회하기 위한 findByEmail()추가

---
+++ src/main/java/com/Login/LOGIN/config/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Login.LOGIN.config.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByEmail(String email); // 이메일을 통해 회원 조회하기 위해서
+}
