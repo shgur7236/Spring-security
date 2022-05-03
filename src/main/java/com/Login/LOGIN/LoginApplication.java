01390c2720ad8ec269bece79ff05ec84d513d5ae
shgur7236 <shgurtns7236@naver.com>
2022-05-03 09:43:43 +0900

    test

--- src/main/java/com/Login/LOGIN/LoginApplication.java
+++ src/main/java/com/Login/LOGIN/LoginApplication.java
@@ -3,7 +3,7 @@ package com.Login.LOGIN;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication //테스트
+@SpringBootApplication //테스트 test
 public class LoginApplication {
 
 	public static void main(String[] args) {
