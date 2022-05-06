a76da2ff764f775bad88ff5bb0c2bb1b438bc9b7
shgur7236 <shgurtns7236@naver.com>
2022-05-06 12:29:58 +0900

    UserInfoDto(email, password, auth) 구현

---
+++ src/main/java/com/Login/LOGIN/web/dto/UserInfoDto.java
@@ -0,0 +1,13 @@
+package com.Login.LOGIN.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+
+    private String email;
+    private String password;
+    private String auth;
+}
