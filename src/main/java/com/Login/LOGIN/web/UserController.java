cc9dacafa1bd17f6ad2d5db875132131b3876323
shgur7236 <shgurtns7236@naver.com>
2022-05-07 13:46:19 +0900

    로그아웃 구현

--- src/main/java/com/Login/LOGIN/web/UserController.java
+++ src/main/java/com/Login/LOGIN/web/UserController.java
@@ -3,8 +3,14 @@ package com.Login.LOGIN.web;
 import com.Login.LOGIN.service.UserService;
 import com.Login.LOGIN.web.dto.UserInfoDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +22,12 @@ public class UserController {
     public String signup(UserInfoDto infoDto){
         userService.save(infoDto);
         return "redirect:/login";
+    }
+
+    @GetMapping("/logout") // logout by GET 요청
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder
+                .getContext().getAuthentication());
+        return "redirect:login";
     }
 }
